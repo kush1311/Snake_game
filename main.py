9be79a6ed052dd013ebd6f4a830cf21cd4653c66
kush1311 <116818040+kush1311@users.noreply.github.com>
2023-06-15 22:37:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,14 +33,13 @@ while game_is_on:
         scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
-
+            scoreboard.reset()
+            snake.reset()
     
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()    
+            scoreboard.reset()
+            snake.reset()
 screen.exitonclick()
